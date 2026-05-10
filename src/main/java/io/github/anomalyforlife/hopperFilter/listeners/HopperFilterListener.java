d397a03f2c02dd17a6763412357b619688325ddb
Cimo - Anomaly <themrsimon1@gmail.com>
2026-05-10 16:08:43 +0200

    fix(hopper-filter): revert

--- src/main/java/io/github/anomalyforlife/hopperFilter/listeners/HopperFilterListener.java
+++ src/main/java/io/github/anomalyforlife/hopperFilter/listeners/HopperFilterListener.java
@@ -7,7 +7,6 @@ import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Hopper;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -23,6 +22,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
 
 import io.github.anomalyforlife.hopperFilter.gui.FilterGui;
 import io.github.anomalyforlife.hopperFilter.gui.FilterGuiHolder;
